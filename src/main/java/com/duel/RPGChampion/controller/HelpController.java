87f843367fecc5fbb2ccd7085ebcf50102e78dd9
TotoNath <ordinaryshowspl@gmail.com>
2024-07-29 21:55:45 +0200

    fixing sonar

--- src/main/java/com/duel/RPGChampion/controller/HelpController.java
+++ src/main/java/com/duel/RPGChampion/controller/HelpController.java
@@ -33,10 +33,8 @@ public class HelpController extends ListenerAdapter implements CommandController
         String command = event.getMessage().getContentRaw();
 
         if (command.equalsIgnoreCase(prefix + "help")) {
-            List<String> allCommands = new ArrayList<>();
-            commandControllers.forEach(controller -> allCommands.addAll(controller.getCommands()));
-
-            sendHelpMessage(event, allCommands, 0);
+            List<String> allCommands = getAllCommands();
+            sendHelpMessage(event.getChannel(), allCommands, 0);
         }
     }
 
@@ -45,9 +43,22 @@ public class HelpController extends ListenerAdapter implements CommandController
         return List.of(prefix + "help : shows all commands");
     }
 
-    private void sendHelpMessage(MessageReceivedEvent event, List<String> commands, int pageIndex) {
-        int totalPages = (int) Math.ceil((double) commands.size() / COMMANDS_PER_PAGE);
+    private List<String> getAllCommands() {
+        List<String> allCommands = new ArrayList<>();
+        commandControllers.forEach(controller -> allCommands.addAll(controller.getCommands()));
+        return allCommands;
+    }
 
+    private void sendHelpMessage(net.dv8tion.jda.api.entities.MessageChannel channel, List<String> commands, int pageIndex) {
+        int totalPages = (int) Math.ceil((double) commands.size() / COMMANDS_PER_PAGE);
+        EmbedBuilder embedBuilder = createEmbedBuilder(commands, pageIndex, totalPages);
+
+        channel.sendMessageEmbeds(embedBuilder.build()).queue(message -> {
+            addReactions(message, pageIndex, totalPages);
+        });
+    }
+
+    private EmbedBuilder createEmbedBuilder(List<String> commands, int pageIndex, int totalPages) {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setTitle("Available Commands (Page " + (pageIndex + 1) + "/" + totalPages + ")");
         embedBuilder.setColor(Color.BLUE);
@@ -58,84 +69,55 @@ public class HelpController extends ListenerAdapter implements CommandController
         for (int i = start; i < end; i++) {
             helpMessage.append(commands.get(i)).append("\n");
         }
-
         embedBuilder.addField("Commands", helpMessage.toString(), false);
+        return embedBuilder;
+    }
 
-        event.getChannel().sendMessageEmbeds(embedBuilder.build()).queue(message -> {
-            if (totalPages > 1) {
-                if (pageIndex > 0) {
-                    message.addReaction(Emoji.fromUnicode(LEFT_ARROW)).queue(); // Emoji gauche (⬅️)
-                }
-                if (pageIndex < totalPages - 1) {
-                    message.addReaction(Emoji.fromUnicode(RIGHT_ARROW)).queue(); // Emoji droit (➡️)
-                }
+    private void addReactions(Message message, int pageIndex, int totalPages) {
+        if (totalPages > 1) {
+            if (pageIndex > 0) {
+                message.addReaction(Emoji.fromUnicode(LEFT_ARROW)).queue();
             }
-        });
+            if (pageIndex < totalPages - 1) {
+                message.addReaction(Emoji.fromUnicode(RIGHT_ARROW)).queue();
+            }
+        }
     }
 
     @Override
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
         Message message = event.retrieveMessage().complete();
         if (message.getAuthor().isBot() && !event.getUser().isBot()) {
-            String[] titleParts = message.getEmbeds().get(0).getTitle().split(" ");
-            int currentPage = 0;
-            int totalPages = 0;
+            int[] pages = getCurrentAndTotalPages(message.getEmbeds().get(0).getTitle());
+            int currentPage = pages[0];
+            int totalPages = pages[1];
 
-            try {
-                currentPage = Integer.parseInt(titleParts[titleParts.length - 1].split("/")[0]) - 1; // Page index position
-                totalPages = Integer.parseInt(titleParts[titleParts.length - 1].split("/")[1].replace(")", ""));
-            } catch (NumberFormatException e) {
-                // Log or handle the error
-                e.printStackTrace();
-                return;
+            List<String> allCommands = getAllCommands();
+
+            if (event.getReaction().getEmoji().getName().equals(LEFT_ARROW) && currentPage > 0) {
+                updateHelpMessage(message, allCommands, currentPage - 1);
+            } else if (event.getReaction().getEmoji().getName().equals(RIGHT_ARROW) && currentPage < totalPages - 1) {
+                updateHelpMessage(message, allCommands, currentPage + 1);
             }
 
-            List<String> allCommands = new ArrayList<>();
-            commandControllers.forEach(controller -> allCommands.addAll(controller.getCommands()));
-
-            if (event.getReaction().getEmoji().getName().equals(LEFT_ARROW)) {
-                if (currentPage > 0) {
-                    updateHelpMessage(message, allCommands, currentPage - 1);
-                }
-            } else if (event.getReaction().getEmoji().getName().equals(RIGHT_ARROW)) {
-                if (currentPage < totalPages - 1) {
-                    updateHelpMessage(message, allCommands, currentPage + 1);
-                }
-            }
-
-            // Retire les réactions de l'utilisateur pour éviter le spam
             event.getReaction().removeReaction(event.getUser()).queue();
         }
     }
 
+    private int[] getCurrentAndTotalPages(String title) {
+        String[] titleParts = title.split(" ");
+        int currentPage = Integer.parseInt(titleParts[titleParts.length - 1].split("/")[0]) - 1;
+        int totalPages = Integer.parseInt(titleParts[titleParts.length - 1].split("/")[1].replace(")", ""));
+        return new int[]{currentPage, totalPages};
+    }
+
     private void updateHelpMessage(Message message, List<String> commands, int pageIndex) {
         int totalPages = (int) Math.ceil((double) commands.size() / COMMANDS_PER_PAGE);
+        EmbedBuilder embedBuilder = createEmbedBuilder(commands, pageIndex, totalPages);
 
-        EmbedBuilder embedBuilder = new EmbedBuilder();
-        embedBuilder.setTitle("Available Commands (Page " + (pageIndex + 1) + "/" + totalPages + ")");
-        embedBuilder.setColor(Color.BLUE);
-
-        StringBuilder helpMessage = new StringBuilder();
-        int start = pageIndex * COMMANDS_PER_PAGE;
-        int end = Math.min(start + COMMANDS_PER_PAGE, commands.size());
-        for (int i = start; i < end; i++) {
-            helpMessage.append(commands.get(i)).append("\n");
-        }
-
-        embedBuilder.addField("Commands", helpMessage.toString(), false);
-
-        // Suppression des réactions existantes
         message.clearReactions().queue(success -> {
-            // Mise à jour du message existant
             message.editMessageEmbeds(embedBuilder.build()).queue(updatedMessage -> {
-                if (totalPages > 1) {
-                    if (pageIndex > 0) {
-                        updatedMessage.addReaction(Emoji.fromUnicode(LEFT_ARROW)).queue(); // Emoji gauche (⬅️)
-                    }
-                    if (pageIndex < totalPages - 1) {
-                        updatedMessage.addReaction(Emoji.fromUnicode(RIGHT_ARROW)).queue(); // Emoji droit (➡️)
-                    }
-                }
+                addReactions(updatedMessage, pageIndex, totalPages);
             });
         });
     }
